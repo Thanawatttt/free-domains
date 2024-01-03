b718e85cb7d85b82ccb1b6eaa0025464d5532e1b
Thanawatttt <89511939+Thanawatttt@users.noreply.github.com>
2024-01-03 18:00:52 +0700

    Create bytebin.is-an.app.js

---
+++ domains/bytebin.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'drive cloud site',
+  domain: 'is-an.app',
+  subdomain: 'bytebin',
+  owner: {
+    email: 'tanawattawillha@hotmail.com',
+    repo: 'https://github.com/Thanawatttt/teledrive',
+  },
+  record: {
+    CNAME: 'tanawattawillha@hotmail.com'
+  },
+  proxy: false
+});
